c2d241acfcac952cdfc09419174650ccda1ed66c
Ryan Gates <ryanvgates@gmail.com>
2017-09-17 23:26:49 -0400

    Fixed the code to leverage all the operators of npm install idb-keyval

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 document.getElementById("add").onclick = AddClick;
+document.getElementById("logKeysToConsole").onclick = () => idbKeyval.keys().then(val => console.log(val));
+document.getElementById("delete").onclick = () => idbKeyval.delete(document.getElementById("key").value);
+document.getElementById("clear").onclick = () => idbKeyval.clear();
 
 function AddClick(e){
     var idbKey = document.getElementById("key").value;
     var idbValue = document.getElementById("value").value;
     idbKeyval.set(idbKey, idbValue)
     .then(() => console.log("setting the key to " + idbKey + " and value to " + idbValue))
-    .catch(err => console.log('It failed!', err));
-    idbKeyval.get(idbKey).then(val => console.log(val));
+    .catch(err => console.log('It failed!', err));    
 };
