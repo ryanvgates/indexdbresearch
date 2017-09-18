84d03fdb6c80aa896a82059ae8ad6a7ec8500e04
Ryan Gates <ryanvgates@gmail.com>
2017-09-17 23:02:44 -0400

    I got read and write working for the indexdb

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-document.getElementById("add").onclick(function(getElementById){
-    var key = document.getElementById("key");
-    var value = document.getElementById("value");
-});
+document.getElementById("add").onclick = AddClick;
+
+function AddClick(e){
+    var idbKey = document.getElementById("key").value;
+    var idbValue = document.getElementById("value").value;
+    idbKeyval.set(idbKey, idbValue)
+    .then(() => console.log("setting the key to " + idbKey + " and value to " + idbValue))
+    .catch(err => console.log('It failed!', err));
+    idbKeyval.get(idbKey).then(val => console.log(val));
+};
